ce9e7cb4157105acb4ab29530706682512bc6749
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-02-28 16:49:50 -0300

    project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/matheus/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
